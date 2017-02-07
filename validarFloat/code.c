224d0c7dc4526a525746b334bb5df71b33ccb14f
brian <brian.alonso.a@gmail.com>
2017-02-07 00:31:13 -0300

    Fix array with low value

--- validarFloat/code.c
+++ validarFloat/code.c
@@ -5,7 +5,7 @@
 #include <float.h>
 int yylineno = 0; //esto en lo da flex en realidad, es el numero de línea del error
                     //acá se lo pongo para que el yyerror sea compatible con el nuestro
-char yytext[30]; //lo mismo que lo de arriba
+char yytext[100]; //lo mismo que lo de arriba
 
 int yyerror(char *msg){
     fprintf(stderr, "At line %d %s in text: %s\n", yylineno, msg, yytext);
@@ -15,7 +15,7 @@ int yyerror(char *msg){
 int validarFloat(char flotante[]) {
     double casteado = atof(flotante);
     casteado = fabs(casteado);
-    char msg[100];
+    char msg[300];
 
     if(casteado < FLT_MIN || casteado > FLT_MAX) {
         sprintf(msg, "ERROR: Float %f fuera de rango. Debe estar entre [1.17549e-38; 3.40282e38]\n", casteado);
@@ -35,7 +35,7 @@ int validarFloat(char flotante[]) {
 int main(){
     double min = pow(-1.17549,-38);
     double max = pow(3.40282,38);
-    validarFloat("-123456789012345678901234567890123456789");
+    validarFloat("-1234567890123456789012345678901234567890123456789");
     validarFloat("-111111111111111111.17549");
     validarFloat("333333333333333.40282");
     validarFloat("-1444444444444.17550");
